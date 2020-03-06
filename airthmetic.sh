9d98b9f342072184611c3259fd7748f08bc3d702
gauravm-bit <gauravwrk01@gmail.com>
2020-03-06 17:10:56 +0530

    ADDED:take three inputs

--- airthmetic.sh
+++ airthmetic.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome to sorting airthmetic computation problem"
 
 read -p "Enter the value of a " a
 read -p "Enter the value of b " b
 read -p "Enter the value of c " c
+
