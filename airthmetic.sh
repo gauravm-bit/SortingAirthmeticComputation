162979ad9c82a257a1abfeec9ceda46cf8e7b878
gauravm-bit <gauravwrk01@gmail.com>
2020-03-07 09:31:02 +0530

    storing results in dictionary

--- airthmetic.sh
+++ airthmetic.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -x
 
+declare -A dict
+
 echo "Welcome to sorting airthmetic computation problem"
 
 read -p "Enter the value of a " a
 read -p "Enter the value of b " b
 read -p "Enter the value of c " c
 
-echo "$a % $b +$c" | bc
+result1= echo "$a + $b * $c" | bc
+result2= echo "$a * $b + $c" | bc
+result3= echo "$c + $a / $b" | bc
+result4= echo "$a % $b + $c" | bc
+
+dict[1]=$result1
+dict[2]=$result2
+dict[3]=$result3
+dict[4]=$result4
+
