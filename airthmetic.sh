66a1b3c6f4aff003bf9a7e9312b4fccb21567969
gauravm-bit <gauravwrk01@gmail.com>
2020-03-07 10:08:25 +0530

    read values from dictionaries into array

--- airthmetic.sh
+++ airthmetic.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 declare -A dict
+declare -a arr
 
 echo "Welcome to sorting airthmetic computation problem"
 
@@ -8,13 +9,18 @@ read -p "Enter the value of a " a
 read -p "Enter the value of b " b
 read -p "Enter the value of c " c
 
-result1= echo "$a + $b * $c" | bc
-result2= echo "$a * $b + $c" | bc
-result3= echo "$c + $a / $b" | bc
-result4= echo "$a % $b + $c" | bc
+result1=`echo "$a + $b * $c" | bc`
+result2=`echo "$a * $b + $c" | bc`
+result3=`echo "$c + $a / $b" | bc`
+result4=`echo "$a % $b + $c" | bc`
 
-dict[1]=$result1
-dict[2]=$result2
-dict[3]=$result3
-dict[4]=$result4
+dict[1]="$result1"
+dict[2]="$result2"
+dict[3]="$result3"
+dict[4]="$result4"
 
+for(( i=1; i<=4; i++ ))
+do
+	arr[i]=${dict[$i]}
+done
+echo ${arr[@]}
